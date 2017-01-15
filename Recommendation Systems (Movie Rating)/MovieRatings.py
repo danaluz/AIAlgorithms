22a44deaf93d0e8f14dc0a295f903feceb1edf98
Dana <dana.luz@hotmail.com.ar>
2017-01-15 04:35:42 -0300

    Goals of the algorith, minor refactoring

--- Recommendation Systems (Movie Rating)/MovieRatings.py
+++ Recommendation Systems (Movie Rating)/MovieRatings.py
@@ -12,31 +12,27 @@ pepe3 = []
 pepe4 = []
 pepe5 = []
 
-# Evaluatins by Movies
-for i in ratings:
-    ratingsColumn = ratings[i]
-    ratingsColumn = ratingsColumn[~np.isnan(ratingsColumn)]
-    mean = (ratingsColumn.sum())/len(ratingsColumn)
-    pepe1.append(round(mean,2))
-    pepe2.append(ratingsColumn.name[:20]+'...')
-    pepe3.append(len(ratingsColumn))
-    counter = collections.Counter(ratingsColumn)
-    percentage = ((counter[4]+counter[5])*100)/len(ratingsColumn)
-    pepe4.append(round(percentage, 2))
-
 # Cleaning some of the data
 ratingsMovieTarget = ratings["260: Star Wars: Episode IV - A New Hope (1977)"]
 comparison1 = ~np.isnan(ratingsMovieTarget)
 # Lenght of comparison
 long = len(ratingsMovieTarget[comparison1])
 
+# Evaluatins by Movies
 for i in ratings:
     ratingsColumn = ratings[i]
     ratingsColumn = ratingsColumn[~np.isnan(ratingsColumn)]
+    mean = (ratingsColumn.sum())/len(ratingsColumn)
+    counter = collections.Counter(ratingsColumn)
+    percentage = ((counter[4]+counter[5])*100)/len(ratingsColumn)
     compare = (comparison1 & ratingsColumn)
     counting = list(filter(lambda x: x == True, compare))
     counting = len(counting)
-    porcent = (counting/long)*100
+    porcent = (counting / long) * 100
+    pepe1.append(round(mean,2))
+    pepe2.append(ratingsColumn.name[:20]+'...')
+    pepe3.append(len(ratingsColumn))
+    pepe4.append(round(percentage, 2))
     pepe5.append(round(porcent, 2))
 
 # Creating a new DataFrame
@@ -47,8 +43,28 @@ df.drop(df.index[[0]], inplace=True)
 # Reordering the Columns
 df = df[['Name', 'Percentages','Mean','Ratings', 'Distance']]
 
+
 #Sort by Percentages
-test = df.sort_values(by='Percentages', ascending=False)
+testPercentages = df.sort_values(by='Percentages', ascending=False)
 
 #Printing..
-print(test)
+print(testPercentages)
+
+#Sort by Mean
+testMean = df.sort_values(by='Mean', ascending=False)
+
+#Printing..
+print(testMean)
+
+#Sort by Ratings
+testRatings = df.sort_values(by='Ratings', ascending=False)
+
+#Printing..
+print(testRatings)
+
+#Sort by Distance
+testDistance = df.sort_values(by='Distance', ascending=False)
+
+#Printing..
+print(testDistance)
+
